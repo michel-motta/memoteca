67bc032552599e10325c17cc3686fb62c3be5a39
Michel Motta <michelmotta.dev@gmail.com>
2022-10-16 22:09:11 -0300

    aula 4

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -6,6 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listar-pensamento.component.css'],
 })
 export class ListarPensamentoComponent implements OnInit {
+  listaPensamentos = [
+    {
+    conteudo: 'Olá!',
+    autoria: 'Maria',
+    modelo: 'modelo2'
+    },
+    {
+      conteudo: 'Tchau',
+      autoria: 'José',
+      modelo: 'modelo1'
+    },
+    {
+      conteudo: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis vitae nisi sed ligula iaculis sodales. In sed erat at orci consequat rhoncus. Vivamus bibendum finibus metus sed placerat. Suspendisse non maximus diam. Phasellus et condimentum nunc. Duis tristique, eros non elementum euismod, quam orci dignissim diam, at cursus urna odio a ipsum. Nunc risus libero, rutrum et hendrerit eget, rutrum non metus. Nunc eu ultrices ex, id egestas metus. Integer lobortis feugiat dui. Morbi in suscipit mi.',
+      autoria: 'João',
+      modelo: 'modelo1'
+    },
+]
+
   constructor() {}
 
   ngOnInit(): void {}
